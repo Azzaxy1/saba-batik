9d037bb4e423d8114303b6b75dfb1820e332a110
Azzaxy <abdurrohmanazis@gmail.com>
2025-01-24 02:25:59 +0700

    feat: add batik men images and update product item page to include new batik men options

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,8 +1,35 @@
+// Batik Men
+import Batik1Salem from "@/assets/img/batik/man/batik3/salem.png";
+import Batik1Biru from "@/assets/img/batik/man/batik3/biru.png";
+import Batik1Hijau from "@/assets/img/batik/man/batik3/hijau.png";
+import Batik1Kuning from "@/assets/img/batik/man/batik3/kuning.png";
+import Batik1Merah from "@/assets/img/batik/man/batik3/merah.png";
+import Batik1Jingga from "@/assets/img/batik/man/batik3/jingga.png";
+
+import Batik2Salem from "@/assets/img/batik/man/batik4/salem.png";
+import Batik2Biru from "@/assets/img/batik/man/batik4/biru.png";
+// import Batik1Hijau from "@/assets/img/batik/man/batik4/hijau.png";
+import Batik2Kuning from "@/assets/img/batik/man/batik4/kuning.png";
+import Batik2Merah from "@/assets/img/batik/man/batik4/merah.png";
+import Batik2Jingga from "@/assets/img/batik/man/batik4/jingga.png";
+
+import Batik5Biru from "@/assets/img/batik/man/batik5/biru.png";
+import Batik5Merah from "@/assets/img/batik/man/batik5/merah.png";
+import Batik5Hijau from "@/assets/img/batik/man/batik5/hijau.png";
+
+// Batik Women
 import Batik4Salem from "@/assets/img/batik/women/batik1_salem.jpg";
 import Batik4Biru from "@/assets/img/batik/women/batik1_biru.jpeg";
 import Batik4Hijau from "@/assets/img/batik/women/batik1_hijau.png";
 import Batik4Kuning from "@/assets/img/batik/women/batik1_kuning.png";
 import Batik4Merah from "@/assets/img/batik/women/batik1_merah.jpg";
+
+import Batik3Salem from "@/assets/img/batik/women/batik2/batik2_salem.png";
+import Batik3Biru from "@/assets/img/batik/women/batik2/batik2_biru.png";
+import Batik3Hijau from "@/assets/img/batik/women/batik2/batik2_hijau.png";
+import Batik3Kuning from "@/assets/img/batik/women/batik2/batik2_kuning.png";
+import Batik3Merah from "@/assets/img/batik/women/batik2/batik2_merah.png";
+import Batik3Jingga from "@/assets/img/batik/women/batik2/batik2_jingga.png";
 
 export const batikWomen = {
   Batik4Salem,
@@ -10,4 +37,27 @@ export const batikWomen = {
   Batik4Hijau,
   Batik4Kuning,
   Batik4Merah,
+  Batik3Salem,
+  Batik3Biru,
+  Batik3Hijau,
+  Batik3Kuning,
+  Batik3Merah,
+  Batik3Jingga,
+};
+
+export const batikMen = {
+  Batik1Salem,
+  Batik1Biru,
+  Batik1Hijau,
+  Batik1Kuning,
+  Batik1Merah,
+  Batik1Jingga,
+  Batik2Salem,
+  Batik2Biru,
+  Batik2Kuning,
+  Batik2Merah,
+  Batik2Jingga,
+  Batik5Biru,
+  Batik5Merah,
+  Batik5Hijau,
 };
